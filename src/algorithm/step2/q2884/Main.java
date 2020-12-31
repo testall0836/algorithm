725b5e471abff6e7dd74dd81c9cfb82fcee87797
testall0836 <testall0836@gmail.com>
2020-12-31 14:23:01 +0900

    clear step2.q2884

---
+++ src/algorithm/step2/q2884/Main.java
@@ -0,0 +1,30 @@
+package algorithm.step2.q2884;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+		String[] inputs = input.split(" ");
+		
+		int input1 = Integer.parseInt(inputs[0]);
+		int input2 = Integer.parseInt(inputs[1]);
+		
+		int total = input1 * 60 + input2;
+		
+		if (total < 45) {
+			total = total - 45 + 1440;
+		} else {
+			total = total - 45;
+		}
+		
+		input1 = total/60;
+		input2 = total%60;
+		System.out.println(input1 + " " + input2);
+	}
+}
