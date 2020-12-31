fc3c0678f766ea5c4b71a59892dfe08fabd7d447
testall0836 <testall0836@gmail.com>
2020-12-31 16:21:39 +0900

    clear step3.q11022

---
+++ src/algorithm/step3/q11022/Main.java
@@ -0,0 +1,26 @@
+package algorithm.step3.q11022;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int cnt = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < cnt; i++) {
+			String input = br.readLine();
+			String[] inputs = input.split(" ");
+			int a = Integer.parseInt(inputs[0]);
+			int b = Integer.parseInt(inputs[1]);
+
+			bw.write("Case #" + (i + 1) + ": " + a + " + " + b + " = " +(a + b) + "\n");
+		}
+		br.close();
+		bw.close();
+	}
+}
