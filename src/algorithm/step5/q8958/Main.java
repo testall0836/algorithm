55ba1b263a6d5f53b89c69556573ac45595446cf
testall0836 <testall0836@gmail.com>
2021-01-05 04:41:32 +0900

    clear step5.q8958

--- src/algorithm/step5/q8958/Main.java
+++ src/algorithm/step5/q8958/Main.java
@@ -11,19 +11,26 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int cnt = Integer.parseInt(br.readLine());
-		int total = 0;
-		
-		for (int i = 0; i < cnt; i++) {
+
+		for (int i = 0; i < cnt; i++) { // 테스트 케이스 개수만큼 입력받기
 			String input = br.readLine();
 			char[] chs = input.toCharArray();
-			
-			for (char ch : chs) {
+			int total = 0;
+			int acc = 0;
+
+			for (char ch : chs) { // 각 입력 케이스 배열로 전환 후 한 자리씩 O, X 체크
 				if ("O".equalsIgnoreCase(Character.toString(ch))) {
-					
+					// 총점 합산하기
+					acc = acc + 1;
+					total = total + acc;
+				} else {
+					// 틀린 문제인 경우 누적값 초기화
+					acc = 0;
 				}
 			}
+			bw.write(total + "\n");
 		}
-		
+
 		bw.close();
 		br.close();
 	}
