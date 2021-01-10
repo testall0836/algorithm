8647d4f7e19d76f2c2dbaf732ad056844c374c0d
testall0836 <testall0836@gmail.com>
2021-01-10 16:54:16 +0900

    clear step7.q10809

---
+++ src/algorithm/step7/q10809/Main.java
@@ -0,0 +1,29 @@
+package algorithm.step7.q10809;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		try {
+			String input = br.readLine();
+			
+			for (int i = 97; i <= 122; i++) {
+				bw.write(input.indexOf(i) + " ");
+			}
+
+			bw.flush();
+			bw.close();
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
