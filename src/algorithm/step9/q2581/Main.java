508bd75bc25b8b9429e763f579869ba3aabefe5a
testall0836 <testall0836@gmail.com>
2021-01-21 23:43:10 +0900

    doing step9.q2581

---
+++ src/algorithm/step9/q2581/Main.java
@@ -0,0 +1,31 @@
+package algorithm.step9.q2581;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 소수
+ * 
+ * 시간 제한 	- 1 초 
+ * 메모리 제한	- 128 MB
+ * 
+ * 소요 시간	- 76 ms
+ * 소요 메모리	- 11580 KB
+ * 
+ * @author testall0836
+ * @date 2021. 1. 21
+ *
+ */
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		
+		
+	}
+}
