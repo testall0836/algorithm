5910d2c93c8c447b9fe9243d04739c690534224d
testall0836 <testall0836@gmail.com>
2021-01-22 23:51:56 +0900

    doing q2581

--- src/algorithm/step9/q2581/Main.java
+++ src/algorithm/step9/q2581/Main.java
@@ -8,11 +8,9 @@ import java.io.OutputStreamWriter;
 /**
  * 소수
  * 
- * 시간 제한 	- 1 초 
- * 메모리 제한	- 128 MB
+ * 시간 제한 - 1 초 메모리 제한 - 128 MB
  * 
- * 소요 시간	- 76 ms
- * 소요 메모리	- 11580 KB
+ * 소요 시간 - 76 ms 소요 메모리 - 11580 KB
  * 
  * @author testall0836
  * @date 2021. 1. 21
@@ -22,10 +20,21 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
+		// 1<= M <= N <= 10000
 		int M = Integer.parseInt(br.readLine());
 		int N = Integer.parseInt(br.readLine());
+		int decimalSum = 0;
+		int minDecimal = 0;
 		
-		
+		for (int i = M; i <= N; i++) {
+			
+			int vs = 3;
+			while (true) {
+				if (i == 1 || i == 2) {
+					continue;
+				}
+			}
+		}
 	}
 }
