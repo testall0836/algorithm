bc587966d201188bcf5607d8b850afccd409fadc
testall0836 <testall0836@gmail.com>
2021-01-25 00:55:12 +0900

    clear step9.q1929

--- src/algorithm/step9/q1929/Main.java
+++ src/algorithm/step9/q1929/Main.java
@@ -11,41 +11,44 @@ import java.io.OutputStreamWriter;
  * 시간 제한	- 2 초 
  * 메모리 제한	- 256 MB
  * 
- * 소요 시간	- ms 
- * 소요 메모리	- KB
+ * 소요 시간	- 212 ms 
+ * 소요 메모리	- 25672 KB
  * 
  * @author testall0836
- * @date 2021. 1. 23
+ * @date 2021. 1. 25
  *
  */
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		long startTime = System.currentTimeMillis();
 		
 		String[] input = br.readLine().split(" ");
 		
 		//  (1 ≤ M ≤ N ≤ 1,000,000)
 		int M = Integer.parseInt(input[0]);
 		int N = Integer.parseInt(input[1]);
+
+		boolean[] isPrime = new boolean[N+1];
 		
-		int pn = N - M;
-		boolean[] isPrime = new boolean[pn+1];
+		isPrime[0] = true;
+		isPrime[1] = true;
 		
-		for (int i = 2; i <=M; i++) {
+		for (int i = 2; i <=N; i++) {
 			if (isPrime[i]) {
 				continue;
 			}
 			
-			for (int j=2*i; j<=M; j+=i) {
+			for (int j=2*i; j<=N; j+=i) {
 				isPrime[j] = true;
 			}
 		}
 		
-		
-		long endTime = System.currentTimeMillis();
-		bw.write(String.valueOf(endTime-startTime));
+		for(int i=M; i<=N; i++) {
+			if (!isPrime[i]) {
+				bw.write(String.valueOf(i) + "\n");
+			}
+		}
 		bw.flush();
 		bw.close();
 		br.close();
