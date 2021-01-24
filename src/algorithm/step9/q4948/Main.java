41ae6d89e7601fe68f0910cb01c91b3968db1338
testall0836 <testall0836@gmail.com>
2021-01-25 01:45:00 +0900

    doing step9.q4948

---
+++ src/algorithm/step9/q4948/Main.java
@@ -0,0 +1,28 @@
+package algorithm.step9.q4948;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 베르트랑 공준
+ * 
+ * 시간 제한	- 1 초 
+ * 메모리 제한	- 256 MB
+ * 
+ * 소요 시간	- ms 
+ * 소요 메모리	- KB
+ * 
+ * @author testall0836
+ * @date 2021. 1. 25
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+	}
+}
