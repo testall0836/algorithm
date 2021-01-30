a48b9a4e4357dd5291ab44b2b84c68c926180c29
testall0836 <testall0836@gmail.com>
2021-01-30 22:23:22 +0900

    doing step10.q10872

---
+++ src/algorithm/step10/q10872/Main.java
@@ -0,0 +1,20 @@
+package algorithm.step10.q10872;
+
+/**
+ * 팩토리얼	 
+ * 
+ * 시간 제한	- 1 초 
+ * 메모리 제한	- 256 MB
+ * 
+ * 소요 시간	- ms 
+ * 소요 메모리	- KB 
+ * 
+ * @author	testall0836
+ * @date	2021. 1. 30
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
