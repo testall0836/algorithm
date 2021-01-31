b4619a894911a29c7a66c9671c8d355a0cc0d97e
testall0836 <testall0836@gmail.com>
2021-01-31 13:42:39 +0900

    clear step10.q10872

--- src/algorithm/step10/q10872/Main.java
+++ src/algorithm/step10/q10872/Main.java
@@ -1,20 +1,47 @@
 package algorithm.step10.q10872;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 /**
  * 팩토리얼	 
  * 
  * 시간 제한	- 1 초 
  * 메모리 제한	- 256 MB
  * 
- * 소요 시간	- ms 
- * 소요 메모리	- KB 
+ * 소요 시간	- 80 ms 
+ * 소요 메모리	- 11568 KB 
  * 
  * @author	testall0836
- * @date	2021. 1. 30
+ * @date	2021. 1. 31
  *
  */
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		int input = Integer.parseInt(br.readLine());
+		input = factorial(input);
+		
+		bw.write(String.valueOf(input));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	static int factorial(int input) {
+		
+		if (input == 1) {
+			return input;
+		} else if (input == 0) {
+			return 1;
+		}
+		
+		input = input * factorial(input-1);
+		
+		return input;
 	}
 }
