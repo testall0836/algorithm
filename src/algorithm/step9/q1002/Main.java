2779772cc65c64013a38ab4b3f8143dc3c2c0750
testall0836 <testall0836@gmail.com>
2021-02-03 23:43:28 +0900

    doing step9.q1002

--- src/algorithm/step9/q1002/Main.java
+++ src/algorithm/step9/q1002/Main.java
@@ -1,5 +1,10 @@
 package algorithm.step9.q1002;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 /**
  * ≈Õ∑ø
  * 
@@ -15,6 +20,9 @@ package algorithm.step9.q1002;
  */
 public class Main {
 	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		
 	}
 }
