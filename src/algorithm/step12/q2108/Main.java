26ee9d7d530694537ed4c10a3c794864be00df5c
testall0836 <testall0836@gmail.com>
2021-02-09 22:58:03 +0900

    doing step12.q2108

---
+++ src/algorithm/step12/q2108/Main.java
@@ -0,0 +1,26 @@
+package algorithm.step12.q2108;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 통계학
+ * 
+ * 시간 제한	- 2 초 
+ * 메모리 제한	- 256 MB
+ * 
+ * 소요 시간	-   ms 
+ * 소요 메모리	-   KB
+ * 
+ * @author	testall0836
+ * @date	2021. 2. 9
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+}
