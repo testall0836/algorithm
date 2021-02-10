d71c0ff38a57c26bf6b47e6a094b575d1b317bae
testall0836 <testall0836@gmail.com>
2021-02-10 20:24:15 +0900

    doing step12.q2108 / mode

--- src/algorithm/step12/q2108/Main.java
+++ src/algorithm/step12/q2108/Main.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 통계학
@@ -15,12 +18,39 @@ import java.io.OutputStreamWriter;
  * 소요 메모리	-   KB
  * 
  * @author	testall0836
- * @date	2021. 2. 9
+ * @date	2021. 2. 10
  *
  */
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int inputCnt = Integer.parseInt(br.readLine());
+		double sum = 0;
+		double avg = 0;
+		int[] nums = new int[inputCnt];
+		Map<Integer, Integer> mode = new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < inputCnt; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+			sum += nums[i];
+			
+			if (mode.containsKey(nums[i])) {
+				int prev = mode.get(nums[i]);
+				mode.put(nums[i], prev + 1);
+			} else {
+				mode.put(nums[i], 1);
+			}
+		}
+		
+		Arrays.sort(nums);
+		
+		avg = sum/inputCnt;
+		bw.write(String.format("%.0f", avg) + "\n");
+		bw.write(nums[inputCnt/2] + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
