4a4d08b5a2e669409e00e88409e71c4428638809
testall0836 <testall0836@gmail.com>
2021-02-11 23:14:43 +0900

    doing step12.q1427

---
+++ src/algorithm/step12/q1427/Main.java
@@ -0,0 +1,28 @@
+package algorithm.step12.q1427;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 소트인사이드
+ * 
+ * 시간 제한	- 2 초 
+ * 메모리 제한	- 128 MB
+ * 
+ * 소요 시간	- ms 
+ * 소요 메모리	- KB
+ * 
+ * @author	testall0836
+ * @date	2021. 2. 11
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+	}
+}
