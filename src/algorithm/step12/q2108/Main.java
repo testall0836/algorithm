6a82d9ba8ce3ce3054cd31260254b47e454519ea
testall0836 <testall0836@gmail.com>
2021-02-11 23:12:21 +0900

    clear step12.q2108

--- src/algorithm/step12/q2108/Main.java
+++ src/algorithm/step12/q2108/Main.java
@@ -4,9 +4,13 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * 통계학
@@ -14,11 +18,11 @@ import java.util.Map;
  * 시간 제한	- 2 초 
  * 메모리 제한	- 256 MB
  * 
- * 소요 시간	-   ms 
- * 소요 메모리	-   KB
+ * 소요 시간	- 1036 ms 
+ * 소요 메모리	- 63120 KB
  * 
  * @author	testall0836
- * @date	2021. 2. 10
+ * @date	2021. 2. 11
  *
  */
 public class Main {
@@ -29,26 +33,61 @@ public class Main {
 		int inputCnt = Integer.parseInt(br.readLine());
 		double sum = 0;
 		double avg = 0;
-		int[] nums = new int[inputCnt];
-		Map<Integer, Integer> mode = new HashMap<Integer, Integer>();
+		int modeValue = -1;
+		List<Integer> numbers = new ArrayList<Integer>();
+		Map<Integer, Integer> modeNum = new HashMap<Integer, Integer>();
 		
 		for (int i = 0; i < inputCnt; i++) {
-			nums[i] = Integer.parseInt(br.readLine());
-			sum += nums[i];
+			numbers.add(Integer.parseInt(br.readLine()));
+			sum += numbers.get(i);
 			
-			if (mode.containsKey(nums[i])) {
-				int prev = mode.get(nums[i]);
-				mode.put(nums[i], prev + 1);
+			if (modeNum.containsKey(numbers.get(i))) {
+				int value = modeNum.get(numbers.get(i));
+				modeNum.put(numbers.get(i), value + 1);
 			} else {
-				mode.put(nums[i], 1);
+				modeNum.put(numbers.get(i), 1);
 			}
 		}
 		
-		Arrays.sort(nums);
+		Collections.sort(numbers);
 		
 		avg = sum/inputCnt;
+		
+		//최빈값 정렬 완료
+		List<Entry<Integer, Integer>> list_entries = new ArrayList<Map.Entry<Integer,Integer>>(modeNum.entrySet());
+		Collections.sort(list_entries, new Comparator<Entry<Integer, Integer>>() {
+			@Override
+			public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+				
+				if (o2.getValue().compareTo(o1.getValue()) > 0) {
+					return o2.getValue().compareTo(o1.getValue());
+				} else if (o2.getValue().compareTo(o1.getValue()) == 0) {
+					return o1.getKey().compareTo(o2.getKey());				
+				}
+				return -1;
+			}
+		});
+		
+		//리스트가 2개 이상인 경우 1번 2번 비교
+		if (list_entries.size() > 1) {
+			//최빈 단독
+			if (list_entries.get(0).getValue() > list_entries.get(1).getValue()) {
+				modeValue = list_entries.get(0).getKey();
+			} else {
+				//동일값 여럿
+				modeValue = list_entries.get(1).getKey();
+			}
+			
+		} else {
+			modeValue = list_entries.get(0).getKey();
+		}
+		
 		bw.write(String.format("%.0f", avg) + "\n");
-		bw.write(nums[inputCnt/2] + "\n");
+		bw.write(numbers.get(inputCnt/2) + "\n");
+		bw.write(modeValue + "\n");
+		bw.write(Collections.max(numbers) - Collections.min(numbers) + "\n");
+
+		
 		bw.flush();
 		bw.close();
 		br.close();
