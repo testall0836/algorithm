610809fb01a64d2aea7face74998d457a9125b10
testall0836 <testall0836@gmail.com>
2021-02-15 22:46:34 +0900

    doing step10.q2447

--- src/algorithm/step10/q2447/Main.java
+++ src/algorithm/step10/q2447/Main.java
@@ -1,6 +1,10 @@
 package algorithm.step10.q2447;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
  * º° Âï±â - 10
@@ -16,9 +20,13 @@ import java.util.Scanner;
  *
  */
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		System.out.println(sc.nextLine());
+		int input = Integer.parseInt(br.readLine());
+		boolean[][] coordinate = new boolean[input][input];
+		
+		
 	}
 }
