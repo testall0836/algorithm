1eac578484853550d748465b7ee0fd68c3e59233
testall0836 <testall0836@gmail.com>
2022-12-25 23:57:12 +0900

    2738 번 문제 풀이

--- src/algorithm/step9/q2738/Main.java
+++ src/algorithm/step9/q2738/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 /**
  * 출처 : https://www.acmicpc.net/problem/2738
@@ -21,22 +22,29 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String firstInput = br.readLine();  // 행렬의 크기 N M 입력
-        Integer[] matrixSize = Stream.of(firstInput.split(" ")).map(Integer::valueOf).toArray(Integer[]::new);
         
-        Integer[][] matrixSum = new Integer[matrixSize[0]][matrixSize[1]];
+        // 행렬 크기 얻음
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());   // 행의 개수
+        int m = Integer.parseInt(st.nextToken());   // 원소 개수
 
-        //입력을 먼저 완료 처리해야 됨, 실패한 로직
-        for ( int i = 0; i < matrixSize[0]; i++ ) {
-            String InputMatrix = br.readLine(); //행렬의 행을 순서대로 입력받음
-            Integer[] matrixNum = Stream.of(InputMatrix.split(" ")).map(Integer::valueOf).toArray(Integer[]::new);
-            for ( int j = 0; j < matrixSize[1]; j++ ) {
-                matrixSum[i][j] += matrixNum[j];
+        int[][] matrixSum = new int[n][m];
+
+        for ( int i = 0; i < n*2; i++ ) {
+
+            String line = br.readLine();
+
+            for ( int j = 0; j < m; j++ ) {
+                matrixSum[i%3][j] += Integer.parseInt(line.split(" ")[j]);
             }
         }
-
-
-        
+     
+        for ( int i = 0; i < n; i ++ ) {
+            for ( int j = 0; j < m; j ++ ) {
+                bw.write(String.valueOf(matrixSum[i][j]) + " ");
+            }
+            bw.write("\n ");
+        }
      
         bw.flush();
         bw.close();
