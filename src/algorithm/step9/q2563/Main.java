f663ab35a43e13f621cf3ee0167f7afa397b4c55
testall0836 <testall0836@gmail.com>
2022-12-29 22:50:36 +0900

    백준 2563 문제 추가

---
+++ src/algorithm/step9/q2563/Main.java
@@ -0,0 +1,29 @@
+package algorithm.step9.q2563;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 출처 : https://www.acmicpc.net/problem/2563
+ * 
+ * 시간 제한 : 1초
+ * 메모리 제한 : 128 MB
+ * 
+ * 
+ * @author testall0836
+ * @date 2022. 12. 29
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        
+        br.close();
+        bw.close();
+    }	
+}
