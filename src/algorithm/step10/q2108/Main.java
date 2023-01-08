32fb9bb7216e39544fb0415aaefa5d997c6e779b
testall0836 <testall0836@gmail.com>
2023-01-08 21:29:17 +0900

    백준 > 2108 문제 진행

---
+++ src/algorithm/step10/q2108/Main.java
@@ -0,0 +1,37 @@
+package algorithm.step10.q2108;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+/**
+ * 출처 : https://www.acmicpc.net/problem/2108
+ * 
+ * 시간 제한 : 1초
+ * 메모리 제한 : 256 MB
+ * 
+ * 
+ * @author testall0836
+ * @date 2023. 1. 8
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] firstInput = br.readLine().split(" ");
+
+     
+        bw.flush();
+        bw.close();
+        br.close();
+    }	
+}
